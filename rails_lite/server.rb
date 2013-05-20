d1e73008e7e356012ef129788e2d7d72d0fe944d
Liz Starin and Cody Zupnick <liz+cody@appacademy.io>
2013-05-20 10:41:03 -0400

    Initial commit

---
+++ rails_lite/server.rb
@@ -0,0 +1,44 @@
+# require 'active_support/core_ext'
+require 'webrick'
+# require 'rails_lite'
+
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+root = '/'
+server = WEBrick::HTTPServer.new :Port => 8080
+trap('INT') { server.shutdown }
+#
+# class MyController < ControllerBase
+#
+#   def go
+#     render_content("hello world!", "text/html")
+#
+#     # after you have sessions going, uncomment:
+# #    session["count"] ||= 0
+# #    session["count"] += 1
+# #    render_content("#{session["count"]}", "text/html")
+#
+#     # after you have template rendering
+# #    render :show
+#   end
+# end
+
+
+server.mount_proc '/' do |req, res|
+  req.content_type = "text/text"
+  res.body = req.path
+  #  MyController.new(req, res).go
+end
+
+server.start
+
+
+
+
+# Write a file, lib/rails_lite/server.rb. Create a new WEBrick::HTTPServer on port 8080. To write your own custom handling of web requests, tell the server to use a proc to serve responses. Use HTTPServer#mount_proc. You give it a root URL and a proc which will be passed the HTTPRequest as well a HTTPResponse to fill out.
+#
+# To start, set the root URL to '/'. Set the HTTPResponse#content_type to text/text (also called the mime type) and the body to the HTTPRequest#path.
+#
+# Hint: WEBrick doesn't like to shut down cleanly. Add this line before you start the server:
